808448f21aecf67931f4752c4b4b2fb48a0ad92a
Afsaneh SametTehrani <afsaneh.samettehrani@essec.edu>
2021-05-26 16:53:24 +0100

    added funtionalities up to 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+# Do you remember why we use .create! instead of just .create? Ask around if you forgot
+# if it fails it will put an error and stops the code
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Red & Green Flat London',
+  address: '1 fton Gens London W3 1DT',
+  description: 'Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 70,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'magic Flat London',
+  address: '13 fton Gens London W3 1DT',
+  description: 'Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 4
+)
